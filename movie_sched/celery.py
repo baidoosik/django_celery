6c742d3c4f14cbb22aa0df1bc536ce61d8020415
baidoosik <qoentlr37@naver.com>
2017-10-09 20:11:08 +0900

    celery setting 완료

--- movie_sched/celery.py
+++ movie_sched/celery.py
@@ -5,9 +5,9 @@ from celery.schedules import crontab
 from django.conf import settings
 
 # set the default django settings module for the 'celery' program
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_scheduler.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_sched.settings')
 
-app = Celery('movie_scheduler')
+app = Celery('movie_sched')
 
 # using a string here means the worker don't have to serialize
 # the configuration object to child process.
@@ -16,13 +16,13 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 #load task modules from all registered Django app configs.
 app.autodiscover_tasks(lambda : settings.INSTALLED_APPS)
 
-# app.conf.update(BROKER_URL=os.environ['REDIS_URL'])
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'])
 
 app.conf.beat_schedule ={
     'get_ocn_schedule': {
-        'task': 'scheduler_core.tasks.save_cj_channel_schedule',
-        'schedule': crontab(minute=30, hour=1),
-        'args': ("OCN", "http://ocn.tving.com/ocn/schedule?startDate="),
+        'task': 'scheduler_core.tasks.startup_news_crawling',
+        'schedule': crontab(minute='5,10,15,20'),
+        # 'args': ("OCN", "http://ocn.tving.com/ocn/schedule?startDate="),
     }
 
 }
